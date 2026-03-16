5cb08b395200aa298df60705a5a6c038c1ac46b7
Zadig <odium@riseup.net>
2026-03-16 14:36:44 +0100

    mise à jour questionnaire ctl

--- controller/form-questionnaire_ctl.php
+++ controller/form-questionnaire_ctl.php
@@ -6,51 +6,88 @@
 
 class Quizz
 {
-    public $errors = [];
-    public array $reponses = [
-        'nom' => null,
-        'prenom' => null,
+    public array $errors = [];
+protected string $nom = '';
+protected string $prenom = '';
+
+    protected array $input = [
         'q1' => null,
+        'q2' => null,
+        'q3' => null,
+        'q4' => null,
+        'q5' => null,
+        'q6' => null,
+        'q7' => null,
+        'q8' => null,
+        'q9' => null,
+        'q10' => null,
     ];
+    protected array $reponses = [
+        'q1' => 4,
+        'q2' => 2,
+        'q3' => 3,
+        'q4' => 3,
+        'q5' => 4,
+        'q6' => 2,
+        'q7' => 2,
+        'q8' => 2,
+        'q9' => 3,
+        'q10' => 2,
+    ];
+    protected int $score;
+
 
     public function __construct()
     {
         // Contrôle de qualité (trouve les erreurs et ajoute les messages dans le tableau $errors)
         if (!empty($_POST)) {
-            $this->reponses['nom'] = htmlspecialchars($_POST['nom']) ?? null;
-            $this->reponses['prenom'] = htmlspecialchars($_POST['prenom']) ?? null;
-            $this->reponses['q1'] = htmlspecialchars($_POST['q1']) ?? null;
+            $this->nom = htmlspecialchars($_POST['nom']) ?? null;
+            $this->prenom = htmlspecialchars($_POST['prenom']) ?? null;
+            $this->input['q1'] = htmlspecialchars($_POST['q1']) ?? null;
+            $this->input['q2'] = htmlspecialchars($_POST['q2']) ?? null;
+            $this->input['q3'] = htmlspecialchars($_POST['q3']) ?? null;
+            $this->input['q4'] = htmlspecialchars($_POST['q4']) ?? null;
+            $this->input['q5'] = htmlspecialchars($_POST['q5']) ?? null;
+            $this->input['q6'] = htmlspecialchars($_POST['q6']) ?? null;
+            $this->input['q7'] = htmlspecialchars($_POST['q7']) ?? null;
+            $this->input['q8'] = htmlspecialchars($_POST['q8']) ?? null;
+            $this->input['q9'] = htmlspecialchars($_POST['q9']) ?? null;
+            $this->input['q10'] = htmlspecialchars($_POST['q10']) ?? null;
 
-            if (!$this->reponses['nom']) {
+
+            if (!$this->input['nom']) {
                 $this->errors['nom'] = "Requis";
-            } else  if (strlen(trim($this->reponses['nom'])) < 3) {
+            } else  if (strlen(trim($this->input['nom'])) < 3) {
                 $this->errors['nom'] = "Au moins 3 caractères";
-            } else if (strlen(trim($this->reponses['nom'])) > 50) {
+            } else if (strlen(trim($this->input['nom'])) > 50) {
                 $this->errors['nom'] = "Moins de 50 caractères";
             }
-            if (!$this->reponses['prenom']) {
+
+            if (!$this->input['prenom']) {
                 $this->errors['prenom'] = "Requis";
-            } else  if (strlen(trim($this->reponses['prenom'])) < 3) {
+            } else  if (strlen(trim($this->input['prenom'])) < 3) {
                 $this->errors['prenom'] = "Au moins 3 caractères";
-            } else if (strlen(trim($this->reponses['prenom'])) > 50) {
+            } else if (strlen(trim($this->input['prenom'])) > 50) {
                 $this->errors['prenom'] = "Moins de 50 caractères";
             }
 
-            if (!$this->reponses['q1']) {
+            if (!$this->input['q1']) {
                 $this->errors['price'] = "Requis";
-            } else if (preg_match('placeholder', $this->reponses['q1']) == 0) { // mettre regex pour les options possibles
+            } else if (preg_match('/^[1-4]$/', $this->input['q1']) == 0) { // Teste que la valeur soit un int entre 1 et 4
                 $this->errors['price'] = "Valeur incorrecte";
+            } else if ($this->input['q1'] === $this->reponses['q1']) {
+                $this->score + 1;
             }
         }
 
         // Si $_POST est vide (début) ou qu'il y a des erreurs, affiche la page du questionnaire
         if (empty($_POST) || !empty($this->errors)) {
-            require './vue/questionnaire_vue.php';
+            require ROOT . '/vue/questionnaire_view.php';
         }
         // Si le $_POST est rempli sans erreurs, envoie les résultats dans la BDD et affiche la page de validation
         else {
-            require './model/table_result.php'; // envoie le produit sur la bdd
-            require './vue/questionnaire_fini_vue.php'; // affiche page succès
+            require ROOT . '/model/table_result.php'; // envoie le produit sur la bdd
+            require ROOT . '/vue/questionnaire_fini_vue.php'; // affiche page succès
         }
     }
 }
