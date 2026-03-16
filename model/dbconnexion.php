930ad7b9b034aa1a4492ab1a9d3b892a43fb8f0c
Johan LE GUENNEC <johan.leguennec20@gmail.com>
2026-03-16 15:43:45 +0100

    acces bdd pour questionnaire de base

--- model/dbconnexion.php
+++ model/dbconnexion.php
@@ -13,7 +13,7 @@ class DBConnexion
     {
         try {
 
-            $this->connexion = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, $_SESSION['DB_USER'], $_SESSION['DB_PASSWORD']);
+            $this->connexion = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
             $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             throw new PDOException("Identifiant ou mot de passe incorrect" . $e->getMessage());
