07aff0368090836a1b19ccb5d106c856755e5c72
Johan LE GUENNEC <johan.leguennec20@gmail.com>
2026-03-16 15:20:38 +0100

    Backoffice

--- view/backoffice_view.php
+++ view/backoffice_view.php
@@ -1,8 +1,28 @@
-<h1>BACKOFFICE</h1>
+<h1>Resultats</h1>
 
-<?php if(isset($error)): ?>
-    <p><?= $error ?></p>
-<?php else: ?>
-    <p>Ajout réussis</p>
-    
-    <?php endif; ?>
+<table>
+    <thead>
+        <tr>
+            <th scope="col">Prénom</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Résultat</th>
+            <th scope="col">Temps</th>
+            <th scope="col">Date de passage</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($results as $result): ?>
+            <tr>
+                <th scope="row"><?= $result['prenom'] ?></th>
+                <td><?= $result['nom'] ?></td>
+                <td><?= $result['resultat'] ?></td>
+                <td><?= $result['temps'] ?></td>
+                <td><?= $result['date'] ?></td>
+
+            </tr>
+
+        <?php endforeach ?>
+    </tbody>
+
+
+</table>
