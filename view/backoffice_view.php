ee97d9a1d98e261bdc590c2c7e2016a0b04ec9f4
Johan LE GUENNEC <johan.leguennec20@gmail.com>
2026-03-16 15:32:41 +0100

    Modification du css pour le back office

--- view/backoffice_view.php
+++ view/backoffice_view.php
@@ -1,28 +1,42 @@
-<h1>Resultats</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<table>
-    <thead>
-        <tr>
-            <th scope="col">Prénom</th>
-            <th scope="col">Nom</th>
-            <th scope="col">Résultat</th>
-            <th scope="col">Temps</th>
-            <th scope="col">Date de passage</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($results as $result): ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="static/CSS/style.css">
+</head>
+
+<body>
+    <h1>Resultats</h1>
+
+    <table>
+        <thead>
             <tr>
-                <th scope="row"><?= $result['prenom'] ?></th>
-                <td><?= $result['nom'] ?></td>
-                <td><?= $result['resultat'] ?></td>
-                <td><?= $result['temps'] ?></td>
-                <td><?= $result['date'] ?></td>
-
+                <th scope="col">Prénom</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Résultat</th>
+                <th scope="col">Temps</th>
+                <th scope="col">Date de passage</th>
             </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($results as $result): ?>
+                <tr>
+                    <td><?= $result['prenom'] ?></th>
+                    <td><?= $result['nom'] ?></td>
+                    <td><?= $result['resultat'] ?></td>
+                    <td><?= $result['temps'] ?></td>
+                    <td><?= $result['date'] ?></td>
 
-        <?php endforeach ?>
-    </tbody>
+                </tr>
+
+            <?php endforeach ?>
+        </tbody>
 
 
-</table>
+    </table>
+</body>
+
+</html>
