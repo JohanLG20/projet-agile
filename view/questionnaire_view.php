05691651ed9136bf48c493f7e255ea439ef44709
Johan LE GUENNEC <johan.leguennec20@gmail.com>
2026-03-17 10:22:50 +0100

    blocage des boutons lors de la validation des résultats + colorisation de la première question

--- view/questionnaire_view.php
+++ view/questionnaire_view.php
@@ -44,17 +44,30 @@
             </div>
         <?php endif; ?>
         <h3>Questions</h3>
-        <p>1. Quel serait le meilleur choix de mot de passe ?
+        <p class="question">1. Quel serait le meilleur choix de mot de passe ?
         </p>
         <?php if (isset($this->errors['q1'])): ?>
             <p><?= $this->errors['q1'] ?></p>
         <?php endif; ?>
-        <input type="radio" name="q1" value="a" <?= $this->input['q1'] == 'a' ? 'checked' : '' ?>>jmmouquet<br>
-        <input type="radio" name="q1" value="b" <?= $this->input['q1'] == 'b' ? 'checked' : '' ?>>02101987<br>
-        <input type="radio" name="q1" value="c" <?= $this->input['q1'] == 'c' ? 'checked' : '' ?>>JmMouquet<br>
-        <input type="radio" name="q1" value="d" <?= $this->input['q1'] == 'd' ? 'checked' : '' ?>>Jm-02#M0uqu3T<br>
+        <label class="<?= $this->questionnaireFinished && $this->input['q1'] == 'a' ? 'incorrect' : '' ?>">
+            <input type="radio" name="q1" value="a" <?= $this->input['q1'] == 'a' ? 'checked' : '' ?>>
+            jmmouquet<br>
+        </label>
 
-        <p>2. Mr. Mouquet est parti en vacances. Afin que ses collègues puissent accéder aux dossiers nécessaires pendant son absence, que doit-il faire ?</p>
+        <label class="<?= $this->questionnaireFinished && $this->input['q1'] == 'b' ? 'incorrect' : '' ?>">
+                    <input type="radio" name="q1" value="b" <?= $this->input['q1'] == 'b' ? 'checked' : '' ?>>02101987<br>
+        </label>
+        
+        <label class="<?= $this->questionnaireFinished && $this->input['q1'] == 'c' ? 'incorrect' : '' ?>">
+            <input type="radio" name="q1" value="c" <?= $this->input['q1'] == 'c' ? 'checked' : '' ?>>JmMouquet<br>
+        </label>
+
+        <label class="<?= $this->questionnaireFinished ? 'correct' : '' ?>">
+            <input type="radio" name="q1" value="d" <?= $this->input['q1'] == 'd' ? 'checked' : '' ?>>Jm-02#M0uqu3T<br>
+        </label>
+
+
+        <p class="question">2. Mr. Mouquet est parti en vacances. Afin que ses collègues puissent accéder aux dossiers nécessaires pendant son absence, que doit-il faire ?</p>
         <?php if (isset($this->errors['q2'])): ?>
             <p class="error"><small><?= $this->errors['q2'] ?></small></p>
         <?php endif; ?>
@@ -63,7 +76,7 @@
         <input type="radio" name="q2" value="c" <?= $this->input['q2'] == 'c' ? 'checked' : '' ?>>Donner son mot de passe à un collègue de confiance.<br>
         <input type="radio" name="q2" value="d" <?= $this->input['q2'] == 'd' ? 'checked' : '' ?>>Ne pas verrouiller son poste de travail.<br>
 
-        <p>3. Quel mot de passe est le plus important ?</p>
+        <p class="question">3. Quel mot de passe est le plus important ?</p>
         <?php if (isset($this->errors['q3'])): ?>
             <p class="error"><small><?= $this->errors['q3'] ?></small></p>
         <?php endif; ?>
@@ -72,7 +85,7 @@
         <input type="radio" name="q3" value="c" <?= $this->input['q3'] == 'c' ? 'checked' : '' ?>>Celui de son compte de messagerie.<br>
         <input type="radio" name="q3" value="d" <?= $this->input['q3'] == 'd' ? 'checked' : '' ?>>Celui de mon compte copain d’avant.<br>
 
-        <p>4. Quel site est le plus sécurisé ?</p>
+        <p class="question">4. Quel site est le plus sécurisé ?</p>
         <?php if (isset($this->errors['q4'])): ?>
             <p class="error"><small><?= $this->errors['q4'] ?></small></p>
         <?php endif; ?>
@@ -82,7 +95,7 @@
         <input type="radio" name="q4" value="d" <?= $this->input['q4'] == 'd' ? 'checked' : '' ?>><code>https://netflx.com</code><br>
 
 
-        <p>5. J’ai reçu un mail m’indiquant un lien, dans le but de faire un don pour l’acteur Brad Pitt actuellement hospitalisé. Que dois-je faire ?</p>
+        <p class="question">5. J’ai reçu un mail m’indiquant un lien, dans le but de faire un don pour l’acteur Brad Pitt actuellement hospitalisé. Que dois-je faire ?</p>
         <?php if (isset($this->errors['q5'])): ?>
             <p class="error"><small><?= $this->errors['q5'] ?></small></p>
         <?php endif; ?>
@@ -91,7 +104,7 @@
         <input type="radio" name="q5" value="c" <?= $this->input['q5'] == 'c' ? 'checked' : '' ?>>Je le transfère à tous mes collègues.<br>
         <input type="radio" name="q5" value="d" <?= $this->input['q5'] == 'd' ? 'checked' : '' ?>>Je supprime ou signale le mail comme phishing.<br>
 
-        <p>6. Comment puis-je me rappeler de tous mes mots de passe ?</p>
+        <p class="question">6. Comment puis-je me rappeler de tous mes mots de passe ?</p>
         <?php if (isset($this->errors['q6'])): ?>
             <p class="error"><small><?= $this->errors['q6'] ?></small></p>
         <?php endif; ?>
@@ -100,7 +113,7 @@
         <input type="radio" name="q6" value="c" <?= $this->input['q6'] == 'c' ? 'checked' : '' ?>>Ecrire tous mes mots de passe sur un papier.<br>
         <input type="radio" name="q6" value="d" <?= $this->input['q6'] == 'd' ? 'checked' : '' ?>>Utiliser mon nom ainsi que mon année de naissance.<br>
 
-        <p>7. Que signifie l’authentification à deux facteurs ?</p>
+        <p class="question">7. Que signifie l’authentification à deux facteurs ?</p>
         <?php if (isset($this->errors['q7'])): ?>
             <p class="error"><small><?= $this->errors['q7'] ?></small></p>
         <?php endif; ?>
@@ -109,7 +122,7 @@
         <input type="radio" name="q7" value="c" <?= $this->input['q7'] == 'c' ? 'checked' : '' ?>>Avoir deux comptes différents.<br>
         <input type="radio" name="q7" value="d" <?= $this->input['q7'] == 'd' ? 'checked' : '' ?>>Changer son mot de passe deux fois par an.<br>
 
-        <p>8. Je pense que mon mot de passe a été compromis, que dois-je faire ?</p>
+        <p class="question">8. Je pense que mon mot de passe a été compromis, que dois-je faire ?</p>
         <?php if (isset($this->errors['q8'])): ?>
             <p class="error"><small><?= $this->errors['q8'] ?></small></p>
         <?php endif; ?>
@@ -118,7 +131,7 @@
         <input type="radio" name="q8" value="c" <?= $this->input['q8'] == 'c' ? 'checked' : '' ?>>Créer un nouveau compte.<br>
         <input type="radio" name="q8" value="d" <?= $this->input['q8'] == 'd' ? 'checked' : '' ?>>Eteindre son ordinateur.<br>
 
-        <p>9. J’ai trouvé une clé USB dans le parking de mon entreprise. Que dois-je faire ?</p>
+        <p class="question">9. J’ai trouvé une clé USB dans le parking de mon entreprise. Que dois-je faire ?</p>
         <?php if (isset($this->errors['q9'])): ?>
             <p class="error"><small><?= $this->errors['q9'] ?></small></p>
         <?php endif; ?>
@@ -127,14 +140,23 @@
         <input type="radio" name="q9" value="c" <?= $this->input['q9'] == 'c' ? 'checked' : '' ?>>Vous la remettez au service informatique.<br>
         <input type="radio" name="q9" value="d" <?= $this->input['q9'] == 'd' ? 'checked' : '' ?>>Vous la jetez à la poubelle.<br>
 
-        <p>Question 10 : Pourquoi faut-il faire les mises à jour de son ordinateur ?</p>
+        <p class="question">Question 10 : Pourquoi faut-il faire les mises à jour de son ordinateur ?</p>
         <?php if (isset($this->errors['q10'])): ?>
             <p class="error"><small><?= $this->errors['q10'] ?></small></p>
         <?php endif; ?>
-        <input type="radio" name="q10" value="a" <?= $this->input['q10'] == 'a' ? 'checked' : '' ?>>Pour changer l’apparence du système.<br>
+        <input type="radio" class="" name="q10" value="a" <?= $this->input['q10'] == 'a' ? 'checked' : '' ?>>Pour changer l’apparence du système.<br>
         <input type="radio" name="q10" value="b" <?= $this->input['q10'] == 'b' ? 'checked' : '' ?>>Pour corriger des failles de sécurité.<br>
         <input type="radio" name="q10" value="c" <?= $this->input['q10'] == 'c' ? 'checked' : '' ?>>Pour ralentir l’ordinateur.<br>
         <input type="radio" name="q10" value="d" <?= $this->input['q10'] == 'd' ? 'checked' : '' ?>>Pour supprimer des fichiers.<br>
+
+        <p class="question">Question 11 : Pour protéger mes usages numériques pro/perso :</p>
+        <?php if (isset($this->errors['q11'])): ?>
+            <p class="error"><small><?= $this->errors['q11'] ?></small></p>
+        <?php endif; ?>
+        <input type="radio" name="q11[]" value="a" <?= $this->input['q11'] == 'a' ? 'checked' : '' ?>>J’utilise un stockage de données professionnelles distinct du stockage de données personnelles.<br>
+        <input type="radio" name="q11[]" value="b" <?= $this->input['q11'] == 'b' ? 'checked' : '' ?>>J’utilise ma connexion professionnelle uniquement pour mes besoins professionnels.<br>
+        <input type="radio" name="q11[]" value="c" <?= $this->input['q11'] == 'c' ? 'checked' : '' ?>>J’utilise mon matériel professionnel pour des besoins personnels.<br>
+        <input type="radio" name="q11[]" value="d" <?= $this->input['q11'] == 'd' ? 'checked' : '' ?>>J’effectue les mises à jour de mes systèmes très régulièrement.<br>
 
 
         <input type="hidden" name="time" id="time">
@@ -142,23 +164,29 @@
     </form>
 </body>
 <?php if (!$this->questionnaireFinished): ?>
-<script>
-    let debut = Date.now();
+    <script>
+        let debut = Date.now();
 
-    document.querySelector("form").addEventListener("submit", function() {
+        document.querySelector("form").addEventListener("submit", function() {
 
-        let fin = Date.now();
-        let timeLeft = Math.floor((fin - debut) / 1000);
+            let fin = Date.now();
+            let timeLeft = Math.floor((fin - debut) / 1000);
 
-        document.getElementById("time").value = timeLeft
-    });
+            document.getElementById("time").value = timeLeft
+        });
 
-    setInterval(function() {
-        let maintenant = Date.now();
-        let time = Math.floor((maintenant - debut) / 1000);
-        document.getElementById("chrono").textContent = time;
-    }, 1000);
-</script>
+        setInterval(function() {
+            let maintenant = Date.now();
+            let time = Math.floor((maintenant - debut) / 1000);
+            document.getElementById("chrono").textContent = time;
+        }, 1000);
+    </script>
+<?php else: ?>
+    <script>
+        let radiosButtons = document.querySelectorAll("input[type='radio']")
+        radiosButtons.forEach((elem) => elem.disabled = true)
+    </script>
 <?php endif; ?>
+
 
 </html>
